f30f2d117b65a326d6963ced445a77b69ee18deb
Jheeseong <gmltjd6801@gmail.com>
2022-08-18 21:05:01 +0900

    비밀 정보 보호

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 const { User } = require('./models/User');
 
+const config = require('./config/key');
+
 const bodyParser = require("body-parser"); //body-parser 사용
 
 app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded 로 된 데이터를 분석해서 가져올 수 있게 한다
@@ -32,15 +34,12 @@ app.listen(port, () => console.log(`${port}포트입니다.`));
 // 몽구스 연결
 const mongoose = require('mongoose');
 mongoose
-    .connect(
-        'mongodb+srv://gmltjd6801:jhs9609@shop0.plf9ueo.mongodb.net/?retryWrites=true',
-        {
+    .connect(config.mongoURI, {
           // useNewUrlPaser: true,
           // useUnifiedTofology: true,
           // useCreateIndex: true,
           // useFindAndModify: false,
-        }
-    )
+    })
     .then(() => console.log('MongoDB conected'))
     .catch((err) => {
       console.log(err);
